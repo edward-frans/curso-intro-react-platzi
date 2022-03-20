caa6c2b4165bd214fab8728e8e02d00e2b2c5a08
edward-frans <edwardftg80@gmail.com>
2022-03-20 14:03:02 -0500

    codigo spaguetti

--- src/App.js
+++ src/App.js
@@ -1,21 +1,77 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {TodoCounter} from './components/TodoCounter.js';
 import {CreateTodoButton} from './components/CreateTodoButton.js';
 import {TodoList} from './components/TodoList.js';
 import {TodoSearch} from './components/TodoSearch.js';
 import {TodoItem} from './components/TodoItem.js';
 
-const defaultTodos = [
-  { text: "Hacer ejercicio", completed: true},
-  { text: "Tomar agua ", completed: false},
-  { text: "Dormir", completed: false}
-
-];
+// const defaultTodos = [
+//   { text: "Hacer ejercicio", completed: true},
+//   { text: "Tomar agua ", completed: false},
+//   { text: "Dormir", completed: false}
+// ];
 
 // import './App.css';
 
+function useLocalStorage(itemName, initialValue){
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [item, setItem] = useState(initialValue);
+
+  useEffect(() => {
+    setTimeout(() => {
+      try {
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
+  
+        if(!localStorageItem){
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
+        } else {
+          parsedItem = JSON.parse(localStorageItem);
+        }
+  
+        setItem(parsedItem);
+        setLoading(false);  
+      } catch (error) {
+        setError(error);
+      }
+
+      
+    }, 2000);
+  })
+
+  const saveItem = (newItem) => {
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      setItem(newItem);
+    } catch (error) {
+      setError(error);
+    }
+  }
+
+  return {
+    item,
+    saveItem,
+    loading,
+    error,
+  };
+}
+
+let loading
+let error
+
 function App() {
-  const [todos, setTodos] = useState(defaultTodos)
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error,
+  }= useLocalStorage("TODOS_V1", []);
+
+  
+
   const [searchValue, setSearchValue] = useState("");
   const completedTodos = todos.filter(todo => todo.completed).length;
   const totalTodos = todos.length;
@@ -34,20 +90,29 @@ function App() {
     })
   }
 
+  
+  
+
+
 
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
+
+  useEffect(() => {
+    console.log("useEffect");
+  }, [totalTodos]);
+
 
   return (
     <>
@@ -60,6 +125,10 @@ function App() {
         setSearchValue={setSearchValue}
       />
       <TodoList>
+        {error && <p>Hubo un error...</p>}
+        {loading && <p>Cargando...</p>}
+        {(!loading && !searchedTodos.length) && <p>Crea tu primer TODO!</p>}
+
         {searchedTodos.map(todo => (
           <TodoItem 
             key={todo.text} 
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,6 +5,7 @@ function TodoItem(props) {
   
   return (
     <li className='TodoItem'>
+      {/* <p> {props.loading} {props.error} </p> */}
       <span className={`Check ${props.completed && "Check-active"}`} onClick={props.onComplete}>√</span>
       <p className={`TodoItem-p ${props.completed && 'TodoItem-p-active'}`}> {props.text} </p>
       <span className={`Delete`} onClick={props.onDelete}>X</span>
